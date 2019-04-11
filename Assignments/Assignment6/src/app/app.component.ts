8619f51f8251747fea233e0304a28c3d4ac4d794
NavinNavi19 <navin007.a@gmail.com>
2019-04-11 06:53:36 +0000

    Assignment 6 Completed

--- Assignments/Assignment6/src/app/app.component.ts
+++ Assignments/Assignment6/src/app/app.component.ts
@@ -1,8 +1,16 @@
 import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"]
 })
-export class AppComponent {}
+export class AppComponent {
+  subscriptions = ["Basic", "Advanced", "Pro"];
+  selecetedSubcription = "Advanced";
+
+  onSubmit(form: NgForm) {
+    console.log(form);
+  }
+}
--- Assignments/Assignment6/src/app/app.module.ts
+++ Assignments/Assignment6/src/app/app.module.ts
@@ -3,14 +3,11 @@ import { NgModule } from "@angular/core";
 import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
-import { ActiveUsersComponent } from "./active-users/active-users.component";
-import { InactiveUsersComponent } from "./inactive-users/inactive-users.component";
-import { UserServices } from "./users.services";
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule],
-  providers: [UserServices],
+  providers: [],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
